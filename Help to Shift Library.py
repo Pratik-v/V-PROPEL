851266a24b51f4996f7bf6cf2ed433112dcbb3c5
PRIYANSHU VYAS <84512213+Priyanshu-Vyas@users.noreply.github.com>
2022-03-07 09:09:15 +0530

    Create Help to Shift Library.py

---
+++ Help to Shift Library.py
@@ -0,0 +1,45 @@
+'''In a village, the local library is to be shifted from one place to the other. Books are to be taken from the old location to the new one, there are no vehicles to help the 
+librarian. Two children Ramu and Somu volunteered to help the librarian in the shifting process. Since there are many books shifting will require many trips. The librarian thinks 
+that the children will become tired over time. So he plans to shift the bigger books first and then the smaller books. To make the shifting process interesting he asks Ramu to 
+carry ‘i’ books and Somu to carry ‘2*i’ books in the odd trips and during even trips Ramu has to carry ‘2*i’ books and Somu has to carry ‘i’ books. Where ‘i’ is the trip number 
+and the trips are numbered from 1.
+
+Given the total number of books in the library, write a C program to find out the number of trips required to shift the books, number of books carried by Ramu, number of books 
+carried by Somu and the name of the child that keep the last book in the new location of the library.
+
+For example, when there are 27 books, it will take totally four trips to complete the shifting, Ramu would carry 16 books, Somu would carry 11 books, and the last book will be 
+placed by Somu. When there are 48 books it will take totally six trips to shift, Ramu would carry 24 books, Somu would carry 24 books, and the last book will be placed by Ramu.
+
+Input Format
+
+First line contains the total number of books in the library, n
+
+Output Format
+
+First line should contain the number of trips taken for shifting
+
+Nex line should contain the number of books carried by Ramu and Somu separated by a tab
+
+Next line should contain the name of the child who carried the last book'''
+
+n=int(input())
+r,s,rs=0,0,[]
+i=1;
+while(sum(rs)<n):
+    if(i%2==0):
+        r,s=r+2*i,s+i
+        rs.append(2*i)
+        rs.append(i)
+    else:
+        r,s=r+i,s+2*i
+        rs.append(i)
+        rs.append(2*i)
+    i+=1
+print(i-1)
+if(sum(rs)-n<rs[-1]):
+    print(r,s-(sum(rs)-n),sep="\t")
+    print('Somu')
+else:
+    print(r-(sum(rs)-n-rs[-1]),s-rs[-1],sep="\t")
+    print('Ramu')
+    
